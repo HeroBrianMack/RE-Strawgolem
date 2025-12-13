44505abedde0b4d6abceb29f63c6076b0aab7740
Hero <herostillplays@gmail.com>
2025-12-13 10:38:32 -0500

    Changing BiPredicate to use a generic for filtering.

--- common/src/main/java/org/hero/strawgolem/golem/api/BiPredicate.java
+++ common/src/main/java/org/hero/strawgolem/golem/api/BiPredicate.java
@@ -1,13 +1,15 @@
 package org.hero.strawgolem.golem.api;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.item.ItemEntity;
 import org.hero.strawgolem.golem.StrawGolem;
 
 @FunctionalInterface
 // could do <Q, T> or something (Honestly it's just specific BiPredicate...)
-public interface BiPredicate {
+public interface BiPredicate<T> {
     /**
      * Runs this operation.
      */
-    boolean filter(StrawGolem golem, BlockPos pos);
+    boolean filter(StrawGolem golem, T pos);
+
 }
